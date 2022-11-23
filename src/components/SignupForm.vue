16d494acaa33bc2c6acd8688cbc1619f08298ab7
Ivan Lukyanov <ivan.lukyanov@aliexpress.ru>
2022-11-23 11:39:53 +0100

    initial

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required v-model="email">
+
+    <label>Password:</label>
+    <input type="password" required v-model="password">
+
+    <label>Role:</label>
+    <select v-model="role">
+        <option value="developer">Web developer</option>
+        <option value="designer">Web designer</option>
+    </select>
+  </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email: '',
+            password: '',
+            role: 'designer',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
